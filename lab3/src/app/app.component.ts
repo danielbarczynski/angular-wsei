43e16df1d7b5f68536c7ec7a30d02f20e486f202
Daniel Barczyński <daniel.barczynski@onet.pl>
2023-05-01 09:58:52 +0200

    init

--- lab3/src/app/app.component.ts
+++ lab3/src/app/app.component.ts
@@ -5,11 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  color: string = '';
-  backgroundColor: string = '';
-  border: string = '';
-  shadow: string = '';
-  text: string = '';
-  button: string = '';
-  block: string = '';
 }
